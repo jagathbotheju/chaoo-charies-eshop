b4984eb6ccdc4d3827b07beffaab78f05cbd800b
Jagath Nalaka <jagathbotheju@gmail.com>
2023-10-23 20:12:07 +0300

    init app with Navbar and Footer

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "E-Shop",
+  description: "e commerce app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} min-h-screen flex flex-col text-slate-700`}
+      >
+        <Navbar />
+        <main className="grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
